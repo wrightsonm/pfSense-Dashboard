5579dbe3b23561f499b117f711117243a34ecfe7
mark <mark@voltnet.co.uk>
2022-02-11 19:39:17 +0000

    Add fluxed enabled dashboard. Changes to telegraf config improve efficiency as pfblocker can increase the cardinality too much and impact on database performance

--- plugins/telegraf_pfifgw.php
+++ plugins/telegraf_pfifgw.php
@@ -1,5 +1,6 @@
 #!/usr/local/bin/php-cgi -f
 <?php
+
 require_once("config.inc");
 require_once("gwlb.inc");
 require_once("interfaces.inc");
@@ -9,34 +10,34 @@ $source = "pfconfig";
 
 $iflist = get_configured_interface_with_descr(true);
 foreach ($iflist as $ifname => $friendly) {
-    $ifinfo =  get_interface_info($ifname);
-    $ifstatus = $ifinfo['status'];
-    $ifconf = $config['interfaces'][$ifname];
-    $ip4addr = get_interface_ip($ifname);
-    $ip4subnet = get_interface_subnet($ifname);
-    $ip6addr = get_interface_ipv6($ifname);
-    $ip6subnet = get_interface_subnetv6($ifname);
-    $realif = get_real_interface($ifname);
-    $mac = get_interface_mac($realif);
+	$ifinfo =  get_interface_info($ifname);
+	$ifstatus = $ifinfo['status'];
+	$ifconf = $config['interfaces'][$ifname];
+	$ip4addr = get_interface_ip($ifname);
+	$ip4subnet = get_interface_subnet($ifname);
+	$ip6addr = get_interface_ipv6($ifname);
+	$ip6subnet = get_interface_subnetv6($ifname);
+	$realif = get_real_interface($ifname);
+	$mac = get_interface_mac($realif);
 
     if (!isset($ifinfo)) {
         $ifinfo = "Unavailable";
-    }
-    if (strtolower($ifstatus) == "up") {
-        $ifstatus = 1;
-    }
-    if (strtolower($ifstatus) == "active") {
-        $ifstatus = 1;
-    }
-    if (strtolower($ifstatus) == "no carrier") {
-        $ifstatus = 0;
-    }
-    if (strtolower($ifstatus) == "down") {
-        $ifstatus = 0;
-    }
-    if (!isset($ifstatus)) {
-        $ifstatus = 2;
-    }
+	}
+	if (strtolower($ifstatus) == "up"){
+		$ifstatus = 1;
+	}
+	if (strtolower($ifstatus) == "active"){
+		$ifstatus = 1;
+	}
+	if (strtolower($ifstatus) == "no carrier"){
+		$ifstatus = 0;
+	}
+	if (strtolower($ifstatus) == "down"){
+		$ifstatus = 0;
+	}
+	if (!isset($ifstatus)){
+		$ifstatus = 2;
+	}
     if (!isset($ifconf)) {
         $ifconf = "Unassigned";
     }
@@ -60,20 +61,21 @@ foreach ($iflist as $ifname => $friendly) {
     }
 
 
-    printf(
-        "interface,host=%s,name=%s,ip4_address=%s,ip4_subnet=%s,ip6_address=%s,ip6_subnet=%s,mac_address=%s,friendlyname=%s,source=%s status=%s\n",
-        $host,
-        $realif,
-        $ip4addr,
-        $ip4subnet,
-        $ip6addr,
-        $ip6subnet,
-        $mac,
-        $friendly,
-        $source,
-        $ifstatus
-    );
+	printf(
+		"interface,host=%s,name=%s,ip4_address=%s,ip4_subnet=%s,ip6_address=%s,ip6_subnet=%s,mac_address=%s,friendlyname=%s,source=%s status=%s\n",
+		$host,
+		$realif,
+		$ip4addr,
+		$ip4subnet,
+		$ip6addr,
+		$ip6subnet,
+		$mac,
+		$friendly,
+		$source,
+		$ifstatus
+	);
 }
+
 
 $gw_array = return_gateways_array();
 //$gw_statuses is not guarranteed to contain the same number of gateways as $gw_array
@@ -81,29 +83,29 @@ $gw_statuses = return_gateways_status(true);
 
 $debug = false;
 
-if ($debug) {
-    print_r($gw_array);
-    print_r($gw_statuses);
+if($debug){
+  print_r($gw_array);
+  print_r($gw_statuses);
 }
 
 foreach ($gw_array as $gw => $gateway) {
 
-    //take the name from the $a_gateways list
-    $name = $gateway["name"];
+	//take the name from the $a_gateways list
+	$name = $gateway["name"];
+	
+	$monitor = $gw_statuses[$gw]["monitorip"];
+	$source = $gw_statuses[$gw]["srcip"];
+	$delay = $gw_statuses[$gw]["delay"];
+	$stddev = $gw_statuses[$gw]["stddev"];
+	$loss = $gw_statuses[$gw]["loss"];
+	$status = $gw_statuses[$gw]["status"];
+	$substatus;
 
-    $monitor = $gw_statuses[$gw]["monitorip"];
-    $source = $gw_statuses[$gw]["srcip"];
-    $delay = $gw_statuses[$gw]["delay"];
-    $stddev = $gw_statuses[$gw]["stddev"];
-    $loss = $gw_statuses[$gw]["loss"];
-    $status = $gw_statuses[$gw]["status"];
-    $substatus;
-
-    $interface = $gateway["interface"];
-    $friendlyname = $gateway["friendlyiface"]; # This is not the friendly interface name so I'm not using it
-    $friendlyifdescr = $gateway["friendlyifdescr"];
-    $gwdescr = $gateway["descr"];
-    $defaultgw = $gateway['isdefaultgw'];
+	$interface = $gateway["interface"];
+	$friendlyname = $gateway["friendlyiface"]; # This is not the friendly interface name so I'm not using it
+	$friendlyifdescr = $gateway["friendlyifdescr"];
+	$gwdescr = $gateway["descr"];
+	$defaultgw = $gateway['isdefaultgw'];
 
     if (!isset($monitor)) {
         $monitor = "Unavailable";
@@ -136,37 +138,37 @@ foreach ($gw_array as $gw => $gateway) {
         $gwdescr = "Unassigned";
     }
 
-    if (isset($gateway['isdefaultgw'])) {
-        $defaultgw = "1";
-    } else {
-        $defaultgw = "0";
-    }
+	if (isset($gateway['isdefaultgw'])) {
+		$defaultgw = "1";
+	} else {
+		$defaultgw = "0";
+	}
 
-    if (isset($gateway['monitor_disable'])) {
-        $monitor = "Unmonitored";
-    }
+		if (isset($gateway['monitor_disable'])) {
+			$monitor = "Unmonitored";
+	}
 
-    // Some earlier versions of pfSense do not return substatus
-    if (isset($gw_statuses[$gw]["substatus"])) {
-        $substatus = $gw_statuses[$gw]["substatus"];
-    } else {
-        $substatus = "N/A";
-    }
+	// Some earlier versions of pfSense do not return substatus
+	if (isset($gw_statuses[$gw]["substatus"])) {
+		$substatus = $gw_statuses[$gw]["substatus"];
+	} else {
+		$substatus = "N/A";
+	}
 
-    printf(
-        "gateways,host=%s,interface=%s,gateway_name=%s monitor=\"%s\",source=\"%s\",defaultgw=%s,gwdescr=\"%s\",delay=%s,stddev=%s,loss=%s,status=\"%s\",substatus=\"%s\"\n",
-        $host,
-        $interface,
-        $name, //name is required as it is possible to have 2 gateways on 1 interface.  i.e. WAN_DHCP and WAN_DHCP6
-        $monitor,
-        $source,
-        $defaultgw,
-        $gwdescr,
-        floatval($delay),
-        floatval($stddev),
-        floatval($loss),
-        $status,
-        $substatus
-    );
+	printf(
+		"gateways,host=%s,interface=%s,gateway_name=%s monitor=\"%s\",source=\"%s\",defaultgw=%s,gwdescr=\"%s\",delay=%s,stddev=%s,loss=%s,status=\"%s\",substatus=\"%s\"\n",
+		$host,
+		$interface,
+		$name,//name is required as it is possible to have 2 gateways on 1 interface.  i.e. WAN_DHCP and WAN_DHCP6
+		$monitor,
+		$source,
+		$defaultgw,
+		$gwdescr,
+		floatval($delay),
+		floatval($stddev),
+		floatval($loss),
+		$status,
+		$substatus
+	);
 };
 ?>
